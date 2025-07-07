dddff551c932f7e478926d775648e60b89c544a7
张鑫 <sp42@qq.com>
2025-07-07 17:17:22 +0800

    init

---
+++ src/test/java/com/ajaxjs/http/FooApi.java
@@ -0,0 +1,9 @@
+package com.ajaxjs.http;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@HttpRequest
+public interface FooApi {
+    @GetMapping("https://www.baidu.com")
+    String testBaidu();
+}
---
+++ src/test/java/com/ajaxjs/http/TestHttpRequest.java
@@ -0,0 +1,12 @@
+package com.ajaxjs.http;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHttpRequest {
+    @Test
+    public void testGet() {
+        FooApi fooApi = HttpRequestBuilder.build(FooApi.class);
+        String result = fooApi.testBaidu();
+        System.out.println(result);
+    }
+}
